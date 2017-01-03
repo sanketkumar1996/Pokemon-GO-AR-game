553c4cbf5426c22c8dcf8b8efdfa4f7785017de6
Sanket Thakur <sanketkumar1996@yahoo.com>
2017-01-04 02:19:12 +0530

    Initial Build Up of game

---
+++ Pokemon Go Game/Assets/pokemon1Script.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class pokemon1Script : MonoBehaviour {
+	public Button attackButton1;
+	public GameObject hose;
+
+	// Use this for initialization
+	void Start () {
+		attackButton1.onClick.AddListener (attackButton1Down);
+		hose.transform.FindChild ("WaterShower").gameObject.SetActive (false);
+	}
+	void attackButton1Down(){
+
+	}
+	IEnumerator wait ()
+	{
+		hose.transform.FindChild ("WaterShower").gameObject.SetActive (true);
+		yield return new WaitForSeconds (2);
+		hose.transform.FindChild ("WaterShower").gameObject.SetActive (false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		StartCoroutine (wait());
+	}
+}
---
+++ Pokemon Go Game/Assets/pokemon2Script.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class pokemon2Script : MonoBehaviour {
+	public Button attackButton2;
+	public GameObject spawnPoint; 
+	public GameObject fireBall;
+	// Use this for initialization
+	void Start () {
+		attackButton2.onClick.AddListener (attackButton2Down);
+	}
+	void attackButton2Down(){
+		fireBall = Instantiate (Resources.Load ("fireMobile"), spawnPoint.transform.position, Quaternion.identity) as GameObject;
+		fireBall.GetComponent<Rigidbody>().AddRelativeForce(spawnPoint.transform.forward * -1000f );
+		Destroy (fireBall, 2);
+	}   
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
